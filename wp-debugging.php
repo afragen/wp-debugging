01525d3467889ce91025e21d10d92cbc47af1c88
Andy Fragen <andy@thefragens.com>
2018-07-13 18:25:12 -0700

    added activation/deactivation hooks to modify wp-config.php

--- wp-debugging.php
+++ wp-debugging.php
@@ -12,9 +12,33 @@
  * Requires PHP:      5.6
  */
 
-define( 'WP_DEBUG', true );
-define( 'WP_DEBUG_LOG', true );
-define( 'WP_DEBUG_DISPLAY', true );
+register_activation_hook(
+	__FILE__, function() {
+		$orig_wp_config = file_get_contents( ABSPATH . 'wp-config.php' );
+		file_put_contents( ABSPATH . 'wp-config-orig.php', $orig_wp_config );
+
+		$new_wp_config = explode( "\n", $orig_wp_config );
+		unset( $new_wp_config[0] );
+
+		$wp_debugging_constants = array(
+			'<?php',
+			'// Add debugging constants',
+			"define( 'WP_DEBUG', true );",
+			"define( 'WP_DEBUG_LOG', true );",
+			"define( 'WP_DEBUG_DISPLAY', true );",
+			null,
+		);
+		$new_wp_config          = array_merge( $wp_debugging_constants, $new_wp_config );
+		$new_wp_config          = implode( "\n", $new_wp_config );
+		file_put_contents( ABSPATH . 'wp-config.php', $new_wp_config );
+	}
+);
+register_deactivation_hook(
+	__FILE__, function() {
+		$orig_wp_config = file_get_contents( ABSPATH . 'wp-config-orig.php' );
+		file_put_contents( ABSPATH . 'wp-config.php', $orig_wp_config );
+	}
+);
 
 require_once __DIR__ . '/vendor/autoload.php';
 
