a93d811f09c709134a3d3cfaffcd8373ba1254e3
Andy Fragen <andy@thefragens.com>
2018-09-08 19:25:56 -0700

    composer update removing composer/installers

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -11,25 +11,9 @@ class ComposerStaticInit27800da18fad6df05bcfb18771626cee
         '35bd6a751b552e7a94ae27bc4c52b33a' => __DIR__ . '/..' . '/afragen/wp-dependency-installer/wp-dependency-installer.php',
     );
 
-    public static $prefixLengthsPsr4 = array (
-        'C' => 
-        array (
-            'Composer\\Installers\\' => 20,
-        ),
-    );
-
-    public static $prefixDirsPsr4 = array (
-        'Composer\\Installers\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/composer/installers/src/Composer/Installers',
-        ),
-    );
-
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
-            $loader->prefixLengthsPsr4 = ComposerStaticInit27800da18fad6df05bcfb18771626cee::$prefixLengthsPsr4;
-            $loader->prefixDirsPsr4 = ComposerStaticInit27800da18fad6df05bcfb18771626cee::$prefixDirsPsr4;
 
         }, null, ClassLoader::class);
     }
