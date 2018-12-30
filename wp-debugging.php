6d3c52bb3389928f78d3897df60584580519385e
Andy Fragen <andy@thefragens.com>
2018-12-30 10:58:33 -0800

    dev bump

--- wp-debugging.php
+++ wp-debugging.php
@@ -3,7 +3,7 @@
  * Plugin Name:       WP Debugging
  * Plugin URI:        https://github.com/afragen/wp-debugging
  * Description:       A support/troubleshooting plugin for WordPress.
- * Version:           1.2.7.4
+ * Version:           1.2.7.5
  * Author:            Andy Fragen
  * License:           MIT
  * Network:           true
