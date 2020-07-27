241ea6c7bc651352afa824c22a2b3cc5e6e69a2b
Andy Fragen <andy@thefragens.com>
2020-07-26 18:24:47 -0700

    remove all attempts at fix for erroneous empty config file

--- src/Settings.php
+++ src/Settings.php
@@ -174,7 +174,6 @@ class Settings {
 	 * @return void
 	 */
 	public function process_filter_constants() {
-		$this->exit( self::$config_path );
 		/**
 		 * Filter to add user define constants.
 		 *
@@ -204,26 +203,6 @@ class Settings {
 		$options       = array_diff( self::$options, $remove_user_defined );
 		self::$options = array_merge( $options, $added_constants );
 		update_site_option( 'wp_debugging', (array) self::$options );
-	}
-
-	/**
-	 * Don't run under certain circumstances.
-	 *
-	 * @return void
-	 */
-	private function exit( $config_path ) {
-		if ( defined( 'WP_CLI' ) && \WP_CLI ) {
-			return;
-		}
-		if ( defined( 'DOING_AJAX' ) && \DOING_AJAX ) {
-			return;
-		}
-		if ( defined( 'DOING_CRON' ) && \DOING_CRON ) {
-			return;
-		}
-		if ( ! trim( \file_get_contents( $config_path ) ) ) {
-			exit( 1 );
-		}
 	}
 
 	/**
