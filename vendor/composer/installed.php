ce46f8c3343b41fba504b2b4bfa1d10e8d46c299
Andy Fragen <andy@thefragens.com>
2023-04-09 13:34:45 -0700

    composer update

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'afragen/wp-debugging',
         'pretty_version' => 'dev-develop',
         'version' => 'dev-develop',
-        'reference' => 'ad06910efc53ad74bf4a2cebe53f77a8ca159d57',
+        'reference' => 'af1e32ea059653072b010e62c09429b0eedf3557',
         'type' => 'wordpress-plugin',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         'afragen/wp-debugging' => array(
             'pretty_version' => 'dev-develop',
             'version' => 'dev-develop',
-            'reference' => 'ad06910efc53ad74bf4a2cebe53f77a8ca159d57',
+            'reference' => 'af1e32ea059653072b010e62c09429b0eedf3557',
             'type' => 'wordpress-plugin',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
@@ -38,8 +38,8 @@
             'dev_requirement' => false,
         ),
         'norcross/debug-quick-look' => array(
-            'pretty_version' => '0.1.8',
-            'version' => '0.1.8.0',
+            'pretty_version' => '0.1.9',
+            'version' => '0.1.9.0',
             'reference' => NULL,
             'type' => 'library',
             'install_path' => __DIR__ . '/../norcross/debug-quick-look',
--- vendor/norcross/debug-quick-look/includes/helpers.php
+++ vendor/norcross/debug-quick-look/includes/helpers.php
@@ -148,6 +148,9 @@ function check_debug_file() {
 	// Set our file.
 	$debug = get_debug_file();
 
+	// Set file if empty.
+	$debug = empty( $debug ) ? @ini_set( 'error_log', \WP_CONTENT_DIR . '/debug.log' ) : $debug;
+
 	// If no file exists at all, create an empty one.
 	if ( false === file_exists( $debug ) ) {
 		file_put_contents( $debug, '' );
